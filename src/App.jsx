fbb90cccba32dc180cd5459d3640a300dc4c4193
Your Name <you@example.com>
2024-09-30 23:08:16 +0200

    added Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <Header></Header>
+      <main></main>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
